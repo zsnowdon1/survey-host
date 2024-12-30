be85088c5c715a37369d47171d4c9f0f4d8bc668
zsnowdon <zacksnowdon5@gmail.com>
2024-12-30 17:11:12 -0500

    Changed db to postresql

--- src/main/java/com/voting/survey_host/config/JdbcConfig.java
+++ src/main/java/com/voting/survey_host/config/JdbcConfig.java
@@ -1,13 +1,36 @@
 package com.voting.survey_host.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class JdbcConfig {
+
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public JdbcConfig(@Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+        this.username = username;
+        this.password = password;
+        this.url = url;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource datasource = new DriverManagerDataSource();
+        datasource.setUrl(url);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        return datasource;
+    }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
